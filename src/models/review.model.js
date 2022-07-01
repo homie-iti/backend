325a4b93ba266922ba3d64ad3c488499000604a4
EngMostafaHussien <108274816+EngMostafaHussien@users.noreply.github.com>
2022-07-01 23:21:12 +0200

    min max add

--- src/models/review.model.js
+++ src/models/review.model.js
@@ -1,32 +1,34 @@
 const mongoose = require("mongoose");
 //some property lose
 const schema = new mongoose.Schema(
-	{
-		_id: {
-			type: mongoose.Types.ObjectId,
-		},
-		agentId: {
-			type: mongoose.Types.ObjectId,
-			ref: "agents",
-			required: [true, "review agentId is required"],
-		},
-		unitId: {
-			type: mongoose.Types.ObjectId,
-			ref: "units",
-			required: [true, "review unitId is required"],
-		},
-		comment: {
-			type: String,
-			required: [true, "review comment is required"],
-		},
-		rating: {
-			type: Number,
-			required: [true, "review rating is required"],
-			min: 1,
-			max: 5,
-		},
-	},
-	{ timestamps: true }
+  {
+    _id: {
+      type: mongoose.Types.ObjectId,
+    },
+    agentId: {
+      type: mongoose.Types.ObjectId,
+      ref: "agents",
+      required: [true, "review agentId is required"],
+    },
+    unitId: {
+      type: mongoose.Types.ObjectId,
+      ref: "units",
+      required: [true, "review unitId is required"],
+    },
+    comment: {
+      type: String,
+      min: "",
+      max: "280",
+      required: [true, "review comment is required"],
+    },
+    rating: {
+      type: Number,
+      required: [true, "review rating is required"],
+      min: 1,
+      max: 5,
+    },
+  },
+  { timestamps: true }
 );
 
 mongoose.model("reviews", schema);
