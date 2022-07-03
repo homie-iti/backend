59b6bdce400a0dfd445189e8b575f73ff7fc79a8
Radkillua <radwahmhus@gmail.com>
2022-07-03 17:23:14 +0200

    recommendation routes updates

---
+++ src/controllers/recommednationsController.js
@@ -0,0 +1,47 @@
+
+const mongoose = require("mongoose");
+require("../models/city.model");
+let City = mongoose.model("cities")
+
+
+
+
+// module.exports.getCities = ((request, response, next) => {
+//     City.find({
+//         $or: [{ cityName: "Cairo" },
+//         { cityName: "Alexandria" },
+//         { cityName: "Sharqia" },
+//         { cityName: "Mansoura" },
+//         { cityName: "Tanta" },
+//         { cityName: "5th Settelment" },
+//         ]
+//     }).select({ cityName: 1, _id: 1 })
+//         .then(data => {
+//             if (data == null) { next(new Error("City is not defined")) }
+//             else {
+//                 response.status(200).json(data)
+//             }
+//         })
+//         .catch(error => {
+//             next(error);
+//         })
+
+// });
+
+
+
+
+module.exports.getCities = ((request, response, next) => {
+    let Number = request.params.citiesNumber
+    City.find({}).limit(Number).select({ cityName: 1, _id: 1 })
+        .then(data => {
+            if (data == null) { next(new Error("City is not defined")) }
+            else {
+                response.status(200).json(data)
+            }
+        })
+        .catch(error => {
+            next(error);
+        })
+
+});
