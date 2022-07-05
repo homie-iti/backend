b30ccffaab697adb7db6f0b3fbe6e442e4c8a868
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-07-05 23:00:03 +0200

    initialized city route

--- src/app.js
+++ src/app.js
@@ -6,12 +6,13 @@ require("dotenv").config();
 
 const unitRoute = require("./routers/unit.route");
 const userRoute = require("./routers/user.route");
-const recommendationsRoute = require("./routers/recommendationsRoute")
+const cityRoute = require("./routers/city.route");
+const recommendationsRoute = require("./routers/recommendationsRoute");
 
 require("./models/address.model");
 require("./models/admin.model");
 require("./models/agent.model");
-require("./models/city.model");
+// require("./models/city.model");
 require("./models/contract.model");
 require("./models/helpQuestion.model");
 require("./models/landlord.model");
@@ -22,35 +23,36 @@ require("./models/user.model");
 const app = express();
 const port = process.env.PORT || 8080;
 
-
 const homieDB_URL = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 mongoose
-  .connect(homieDB_URL)
-  .then(() => {
-    app.listen(port, () => {
-      console.log("App listens on port", port);
-    });
-  })
-  .catch((error) => {
-    console.log("DB Connection Error", error);
-  });
+	.connect(homieDB_URL)
+	.then(() => {
+		app.listen(port, () => {
+			console.log("App listens on port", port);
+		});
+	})
+	.catch((error) => {
+		console.log("DB Connection Error", error);
+	});
 
 app.use(cors());
 app.use(morgan(":method :url :status - :response-time ms"));
+app.use(express.json());
 
 app.use(unitRoute);
 app.use(userRoute);
+app.use(cityRoute);
 app.use(recommendationsRoute);
 
 // not-found middleware
 app.use((request, response, next) => {
-  // throw new Error("very big error"); //throwing an error causes the error handling middleware to work
-  response.status(404).json({ message: "Endpoint not found." });
+	// throw new Error("very big error"); //throwing an error causes the error handling middleware to work
+	response.status(404).json({ message: "Endpoint not found." });
 });
 
 // handling errors middleware
 app.use((error, request, response, next) => {
-  response
-    .status(error.status || 500)
-    .json({ message: "Internal Error", details: error.message });
+	response
+		.status(error.status || 500)
+		.json({ message: "Internal Error", details: error.message });
 });
---
+++ src/controllers/city.controller.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+require("../models/city.model");
+
+let citiesCollection = mongoose.model("cities");
+
+// let cityModel = require("../models/unit.model");
+
+module.exports.getAllCities = (request, response, next) => {
+	response.status(200).json({ data: [{ department: 1 }, { department: 2 }] });
+};
+
+module.exports.getCityUnits = (request, response, next) => {
+	const { id: cityId } = request.params;
+
+	citiesCollection
+		.findOne({ _id: cityId }, { _id: 0 })
+		.populate({
+			path: "units",
+			select: { dailyPrice: 1, estateType: 1, images: 1 },
+		})
+		.then((data) => {
+			// console.log(data);
+			if (data == null) next(new Error("City not found"));
+			response.status(200).json(data);
+		})
+		.catch((error) => {
+			next(error);
+		});
+
+	// response.status(200).json({ data: [{ department: 1 }, { department: 2 }] });
+};
+
+// const data = await ClassModel.findById(request.params.id, {
+// 	children: 1,
+// }).populate({
+// 	path: "children",
+// 	select: { _id: 0, fullName: 1, age: 1, level: 1 },
+// });
