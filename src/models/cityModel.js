3383c9720486e51a510aa71f6e12772efae22573
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-07-06 19:07:34 +0200

    added basic creade,read and delete for cities and its units

--- src/models/cityModel.js
+++ src/models/cityModel.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-	_id: {
-		type: mongoose.Types.ObjectId,
-	},
-
 	cover: {
 		type: String,
 	},
+
 	name: {
 		type: String,
 		required: [true, "city cityName is required"],
