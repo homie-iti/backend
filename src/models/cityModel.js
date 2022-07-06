af076832b892ed597815a3e2e4dff795acc192a4
EngMostafaHussien <108274816+EngMostafaHussien@users.noreply.github.com>
2022-07-06 15:59:15 +0200

    admin route & controller

--- src/models/cityModel.js
+++ src/models/cityModel.js
@@ -1,25 +1,25 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-	_id: {
-		type: mongoose.Types.ObjectId,
-	},
+  _id: {
+    type: mongoose.Types.ObjectId,
+  },
 
-	cover: {
-		type: String,
-	},
-	name: {
-		type: String,
-		required: [true, "city cityName is required"],
-	},
-	// you need sure
-	//{
-	units:
-		// type:
-		[{ type: mongoose.Types.ObjectId, ref: "units" }],
-	// ,
-	// required: [true, "city units is required"], TODO ask the team for their opinion
-	// },
+  cover: {
+    type: String,
+  },
+  cityName: {
+    type: String,
+    required: [true, "city cityName is required"],
+  },
+  // you need sure
+  //{
+  units:
+    // type:
+    [{ type: mongoose.Types.ObjectId, ref: "units" }],
+  // ,
+  // required: [true, "city units is required"], TODO ask the team for their opinion
+  // },
 });
 
 mongoose.model("cities", schema);
