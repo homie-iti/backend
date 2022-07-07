cf72d1f1131fd36a5d6505f794e5e397fbe86ecb
safia-elnakhal <safiaelnkhalgmail.com>
2022-07-07 15:16:02 +0200

    some update  in file==>validtion .js

--- src/middlewares/validtion.js
+++ src/middlewares/validtion.js
@@ -24,10 +24,10 @@ const helpQuestionDeleteValidtion = [
 const userPostValidtion = [
     //body("id").isMongoId().withMessage("user id should be a number"),
     body("fullName").isAlpha('en-US', { ignore: " " }).withMessage("user name should be characters"),
-    body("age").isNumeric({ min: 18 }).withMessage("user age is not valid"),
+    body("age").isNumeric({ min: 18 }).withMessage("userAge is not valid"),
     body("gender").isIn(['male', 'female']).withMessage("user gender is not valid"),
-    body("address").isObject().withMessage("user address is not valid"),
-    body("email").isEmail().withMessage("user email is not valid"),
+    body("address").isObject().withMessage("userAddress is not valid"),
+    body("email").isEmail().withMessage("userEmail is not valid"),
     body("phone").isAlphanumeric().withMessage("userPhone should be characters"),
     body("password").isString({ min: 8 }).isLength({ min: 8 }).withMessage("password cannot be less than 8 characters"),
     //body("national_id ").isNumeric().withMessage("user national_id  is not valid"),
