44551de1ea7c16c53c03d6303da24d11196de895
EngMostafaHussien <108274816+EngMostafaHussien@users.noreply.github.com>
2022-07-17 19:34:44 +0200

    login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -6,40 +6,51 @@ let User = mongoose.model("users");
 const jwt = require("jsonwebtoken");
 
 module.exports.login = (request, response, next) => {
-  Admin.findOne(
-    {
+  if (
+    User.findOne({
       email: request.body.email,
       password: request.body.password,
-    },
-  )
-    .then((data) => {
+    })
+  ) {
+    then((data) => {
       if (!data) {
         let error = new Error("email or password incorrect");
         error.status = 401;
         throw error;
-      } else if (data.role == "user") {
-        let token = jwt.sign(
-          {
-            id: data._id,
-            role: "admin",
-          },
-          process.env.secret,
-          { expiresIn: "1h" }
-        );
-        response.status(200).json({ token, message: "login" });
-      } else if (data.role == "user") {
-        let token = jwt.sign(
-          {
-            id: data._id,
-            role: "user",
-          },
-          process.env.secret,
-          { expiresIn: "1h" }
-        );
-        response.status(200).json({ token, message: "login" });
       }
+      let token = jwt.sign(
+        {
+          id: data._id,
+          role: "User",
+        },
+        process.env.secret,
+        { expiresIn: "1h" }
+      );
 
       response.status(200).json({ token, message: "login" });
+    }).catch((error) => next(error));
+  } else if (
+    Admin.findOne({
+      email: request.body.email,
+      password: request.body.password,
     })
-    .catch((error) => next(error));
+  ) {
+    then((data) => {
+      if (!data) {
+        let error = new Error("email or password incorrect");
+        error.status = 401;
+        throw error;
+      }
+      let token = jwt.sign(
+        {
+          id: data._id,
+          role: "Admin",
+        },
+        process.env.secret,
+        { expiresIn: "1h" }
+      );
+
+      response.status(200).json({ token, message: "login" });
+    }).catch((error) => next(error));
+  }
 };
