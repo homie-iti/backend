6548cba0155f7fa63787f53f8bab4243ee1bff28
maryamragab <mariam.relshafei@yahoo.com>
2022-07-21 21:10:31 +0200

    correct spelling error

--- src/routers/helpQuestionRoute.js
+++ src/routers/helpQuestionRoute.js
@@ -1,28 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const validationMW = require("../middlewares/validationMW");
-const {  helpQuestionPostValidtion , helpQuestionUpdateValidtion,helpQuestionDeleteValidtion } = require("../middlewares/validtion")
+const {
+  helpQuestionPostValidation,
+  helpQuestionUpdateValidation,
+  helpQuestionDeleteValidation,
+} = require("../middlewares/validtion");
 const helpController = require("./../controllers/helpQuestionController");
 
-router.route("/helpQuestion")
+router
+  .route("/helpQuestion")
 
-    .get(helpController.getAllQuestion)
+  .get(helpController.getAllQuestion)
 
-    .post(helpQuestionPostValidtion, validationMW, helpController.createQuestion)
+  .post(helpQuestionPostValidation, validationMW, helpController.createQuestion)
 
-    .put(helpQuestionUpdateValidtion, validationMW, helpController.updateHelpQuestion)
+  .put(
+    helpQuestionUpdateValidation,
+    validationMW,
+    helpController.updateHelpQuestion
+  );
 
 // router.route("/helpQuestion/all")
 //     .delete(helpController.deleteAllQuestion)
 
-router.route("/helpQuestion/many")
-    .delete(helpController.deleteManyQuestion)
+router.route("/helpQuestion/many").delete(helpController.deleteManyQuestion);
 
+router
+  .route("/helpQuestion/:id")
+  .get(helpController.getQuestionById)
 
-router.route("/helpQuestion/:id")
-    .get(helpController.getQuestionById)
-
-    .delete( helpQuestionDeleteValidtion, validationMW ,helpController.deleteQuestion)
-
+  .delete(
+    helpQuestionDeleteValidation,
+    validationMW,
+    helpController.deleteQuestion
+  );
 
 module.exports = router;
