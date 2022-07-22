e3330042d4250ac570d13546da22ff7ef839122e
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-07-22 15:41:16 +0200

    commented user route in app.js as its not working yet

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const unitRoute = require("./routers/unitRoute");
-const userRoute = require("./routers/userRoute");
+// const userRoute = require("./routers/userRoute");
 const cityRoute = require("./routers/cityRoute");
 const recommendationsRoute = require("./routers/recommendationsRoute");
 const agentRoute = require("../src/routers/agentRoute");
@@ -31,15 +31,15 @@ const port = process.env.PORT || 8080;
 
 const homieDB_URL = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 mongoose
-  .connect(homieDB_URL)
-  .then(() => {
-    app.listen(port, () => {
-      console.log("App listens on port", port);
-    });
-  })
-  .catch((error) => {
-    console.log("DB Connection Error", error);
-  });
+	.connect(homieDB_URL)
+	.then(() => {
+		app.listen(port, () => {
+			console.log("App listens on port", port);
+		});
+	})
+	.catch((error) => {
+		console.log("DB Connection Error", error);
+	});
 
 app.use(cors());
 app.use(morgan(":method :url :status - :response-time ms"));
@@ -49,7 +49,7 @@ app.use(adminRoute);
 app.use(searchRoute);
 app.use(agentRoute);
 app.use(unitRoute);
-app.use(userRoute);
+// app.use(userRoute);
 app.use(cityRoute);
 app.use(recommendationsRoute);
 app.use(contractRoute);
@@ -58,13 +58,13 @@ app.use(helpRoute);
 
 // not-found middleware
 app.use((request, response, next) => {
-  // throw new Error("very big error"); //throwing an error causes the error handling middleware to work
-  response.status(404).json({ message: "Endpoint not found." });
+	// throw new Error("very big error"); //throwing an error causes the error handling middleware to work
+	response.status(404).json({ message: "Endpoint not found." });
 });
 
 // handling errors middleware
 app.use((error, request, response, next) => {
-  response
-    .status(error.status || 500)
-    .json({ message: "Internal Error", details: error.message });
+	response
+		.status(error.status || 500)
+		.json({ message: "Internal Error", details: error.message });
 });
