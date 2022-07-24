2f46bde88cf0cb96e3032035c7d5f694b944da34
maryamragab <mariam.relshafei@yahoo.com>
2022-07-24 11:37:24 +0200

    adding resetLink property to user model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -11,44 +11,44 @@ const schema = new mongoose.Schema(
             default: false,
         },
 
-		isAgent: {
-			type: Boolean,
-			default: false,
-		},
-    fullName: {
-      type: String,
-      required: [true, "user fullName is required"],
-    },
-    age: {
-      type: Number,
-      min: 18,
-      required: [true, "user age is required"],
-    },
-    email: {
-      type: String,
-      unique: true,
-      required: [true, "user email is required"],
-      // match:{}
-    },
-    gender: {
-      type: String,
-      enum: ["male", "female"],
-      required: [true, "user gender is required"],
-    },
-    password: {
-      type: String,
-      required: [true, "user password is required"],
-    },
-    phone: { type: String, required: [true, "user phone is required"] },
-    national_id: {
-      type: Number,
-      required: [true, "user national_id is required"],
-      unique: true,
-    },
-    image: {
-      type: String,
-      default:"\\uploads\\users\\user.jpg"
-    },
+        isAgent: {
+            type: Boolean,
+            default: false,
+        },
+        fullName: {
+            type: String,
+            required: [true, 'user fullName is required'],
+        },
+        age: {
+            type: Number,
+            min: 18,
+            required: [true, 'user age is required'],
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: [true, 'user email is required'],
+            // match:{}
+        },
+        gender: {
+            type: String,
+            enum: ['male', 'female'],
+            required: [true, 'user gender is required'],
+        },
+        password: {
+            type: String,
+            required: [true, 'user password is required'],
+        },
+        phone: { type: String, required: [true, 'user phone is required'] },
+        national_id: {
+            type: Number,
+            required: [true, 'user national_id is required'],
+            unique: true,
+        },
+        image: {
+            type: String,
+            default: '\\uploads\\users\\user.jpg',
+        },
 
         address: addressSchema,
 
@@ -58,6 +58,10 @@ const schema = new mongoose.Schema(
                 ref: 'units',
             },
         ],
+        resetLink: {
+            data: String,
+            default: '',
+        },
     },
     { timestamps: true }
 )
