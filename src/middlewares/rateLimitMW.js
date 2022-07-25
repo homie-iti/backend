9f93dce803f229195ed74e6097bd9eca9baa0288
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-07-25 12:05:49 +0200

    updated some vars naming in rateLimitMW

--- src/middlewares/rateLimitMW.js
+++ src/middlewares/rateLimitMW.js
@@ -1,23 +1,17 @@
 const rateLimit = require('express-rate-limit')
 
-function limitingHandler(errMessage) {
-    // const errMessage =
-    // 'Too many accounts creation trials from this IP, please try again after an hour.'
+function generateHandler(errMessage) {
     return (request, response, next, options) => {
-        // const errMessage =
-        //     'Too many accounts creation trials from this IP, please try again after an hour.'
         const error = new Error(errMessage)
         error.status = options.statusCode
-
         next(error)
-        // response.status(options.statusCode).send(options.message)
     }
 }
 
 module.exports.apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes * 60 seconds * 1000 milliseconds
     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-    handler: limitingHandler(
+    handler: generateHandler(
         'Too many requests from this IP, please try again after an 15 minutes.'
     ),
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
@@ -27,7 +21,7 @@ module.exports.apiLimiter = rateLimit({
 module.exports.accountCreationLimiter = rateLimit({
     windowMs: 60 * 60 * 1000, // 1 hour = 60 minutes * 60 seconds * 1000 milliseconds
     max: 5, // Limit each IP to 5 create account requests per `window` (here, per hour)
-    handler: limitingHandler(
+    handler: generateHandler(
         'Too many accounts creation trials from this IP, please try again after an hour.'
     ),
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
