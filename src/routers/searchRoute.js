c098c37c5b952da0b627335998c47cfa90817b95
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-07-27 12:59:48 +0200

    fixed search Route

--- src/routers/searchRoute.js
+++ src/routers/searchRoute.js
@@ -4,12 +4,14 @@ const search = require('../controllers/searchController')
 
 const validationMW = require('../middlewares/validationMW')
 
-router.route("/search").get(
-  [query("cityname").isAlpha().withMessage("cityname should be string")],
-  validationMW,
-  search.getCityByName
-);
+const router = express.Router()
 
-
+router
+    .route('/search')
+    .get(
+        [query('cityname').isAlpha().withMessage('cityname should be string')],
+        validationMW,
+        search.getCityByName
+    )
 
 module.exports = router
