f45ef7a3556ebc85522e2d5620855c8551e45c04
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-07-27 13:47:57 +0200

    connected to atlas cloud db

--- src/app.js
+++ src/app.js
@@ -34,9 +34,17 @@ const signupRoute = require('./routers/signupRoute')
 const app = express()
 const port = process.env.PORT || 8080
 
-const homieDB_URL = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+let dbURL
+if (process.env.ENV === 'prod' || process.env.ENV === 'testProd')
+    dbURL = `mongodb+srv://${process.env.ATLAS_DB_USER}:${process.env.ATLAS_DB_PASSWORD}@cluster0.7du11.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+else
+    dbURL = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+
 mongoose
-    .connect(homieDB_URL)
+    .connect(dbURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => {
         app.listen(port, () => {
             console.log('App listens on port', port)
