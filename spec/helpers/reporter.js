f80d934708b739bce2cc2f6be1f90175f06b7eda
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-07-27 10:37:43 +0200

    installeed supertest & added basic test

--- spec/helpers/reporter.js
+++ spec/helpers/reporter.js
@@ -1,10 +1,14 @@
-const SpecReporter = require("jasmine-spec-reporter").SpecReporter;
+const { SpecReporter } = require('jasmine-spec-reporter')
 
-jasmine.getEnv().clearReporters();
+jasmine.getEnv().clearReporters() // remove default reporter logs
 jasmine.getEnv().addReporter(
-	new SpecReporter({
-		spec: {
-			displayPending: true,
-		},
-	})
-);
+    new SpecReporter({
+        // add jasmine-spec-reporter
+        spec: {
+            displayPending: true,
+        },
+        summary: {
+            displayDuration: false,
+        },
+    })
+)
