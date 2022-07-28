434cb4e5d17795eb69ab924977f0dca52b0faf6c
maryamragab <mariam.relshafei@yahoo.com>
2022-07-29 00:14:16 +0200

    adding validations for forget&reset password

--- src/routers/forgetPasswordRoute.js
+++ src/routers/forgetPasswordRoute.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { body } = require('express-validator')
 
 const router = express.Router()
 
@@ -8,7 +9,23 @@ const {
 } = require('../controllers/forgetPasswordController')
 const validationMW = require('../middlewares/validationMW')
 
-router.put('/forgetPassword', forgetPassword)
-router.put('/resetPassword', resetPassword)
+router.put(
+    '/forgetPassword',
+    [body('email').isEmail().withMessage('Please enter valid email')],
+    validationMW,
+    forgetPassword
+)
+router.put(
+    '/resetPassword',
+    [
+        body('newPassword')
+            .isStrongPassword()
+            .withMessage('Please enter strong password')
+            .isLength({ min: 8 })
+            .withMessage('Password should be at least 6 characters..'),
+    ],
+    validationMW,
+    resetPassword
+)
 
 module.exports = router
