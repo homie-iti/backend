40e223a1685fc8b58cdbf8cda7bcee0d6cd4b8a3
EngMostafaHussien <108274816+EngMostafaHussien@users.noreply.github.com>
2022-08-02 01:04:52 +0200

    find edit

--- src/controllers/filtringandsortingController.js
+++ src/controllers/filtringandsortingController.js
@@ -56,7 +56,7 @@ module.exports.filteredUnit = async (request, response, next) => {
         }
 
         const data = await city
-            .find({ _id: request.params._id })
+            .findOne({ _id: request.params._id })
             .populate({ path: 'units', match: filter })
             .sort(sort)
         response.status(200).json({ data })
