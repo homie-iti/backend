2cf8cbb9083678e03ab722ed380d52570499c198
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-08-02 12:38:01 +0200

    fixed the apartment images grabber

--- src/utilities/dbSeeder/apiDataGrabber.js
+++ src/utilities/dbSeeder/apiDataGrabber.js
@@ -13,15 +13,17 @@ async function generateAvatarImage() {
     return `https://this-person-does-not-exist.com${imageSrc}`
 }
 
-async function generateApartmentImage() {
+async function generateApartmentImage(page = 1, query = '') {
     // https://api.pexels.com/v1/search?query=apartment inside
-    const apiURL =
-        'https://api.pexels.com/v1/search?per_page=80&orientation=landscape&query='
-    const query = 'apartment inside'
+    // query.replace(/ /g, '%20')
+    const apiURL = `https://api.pexels.com/v1/search?per_page=80&orientation=landscape&page=${page}&query=${query}`
+    console.log(apiURL)
+    // console.l
     const options = { headers: { Authorization: appConfig.pexelsKey } }
-    const imageSrc = await getData(apiURL + query, options)
-    console.log(imageSrc)
-    // return `https://this-person-does-not-exist.com${imageSrc}`
+    console.log(options)
+    const { photos } = await getData(apiURL + query, options)
+    console.log(photos)
+    return photos
 }
 
 module.exports = { generateAvatarImage, generateApartmentImage }
