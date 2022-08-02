9c2c6fc0dde36ce65603717fa591ae05369c482f
EngMostafaHussien <108274816+EngMostafaHussien@users.noreply.github.com>
2022-08-02 02:46:35 +0200

    edit

--- src/routers/filtringandsortingRoute.js
+++ src/routers/filtringandsortingRoute.js
@@ -1,15 +1,8 @@
-const express = require("express");
-const router = express.Router();
-const { body, param, query } = require('express-validator');
-const filtringsController = require("../controllers/filtringandsortingController");
+const express = require('express')
+const router = express.Router()
+const { body, param, query } = require('express-validator')
+const filtringsController = require('../controllers/filtringandsortingController')
 
+router.route(`/Homie`).get(filtringsController.filteredUnit)
 
-
-router.route(`/Homie/:id`)
-    .get(filtringsController.filteredUnit)
-
-
-
-
-
-module.exports = router;
+module.exports = router
