e47206f8bde643b25e4e866a12e1a0f513774729
maryamragab <mariam.relshafei@yahoo.com>
2022-08-05 10:28:28 +0200

    create separate files for validations

---
+++ src/routers/validations/unitValidations.js
@@ -0,0 +1,107 @@
+const { body } = require('express-validator')
+
+const addUnitValidations = [
+    body('landlordId').isMongoId().withMessage('landlordId Must Be ObjectId'),
+    body('cityId').isMongoId().withMessage('cityId Must Be ObjectId'),
+    body('estateType')
+        .isIn(['studio', 'shared-room', 'single-room', 'apartment'])
+        .withMessage(
+            'EstateType Must Be (Studio||Shared-Room||Single-Room||Apartment)'
+        ),
+    body('allowedGender')
+        .isIn(['female', 'male', 'any'])
+        .withMessage('allowedGender Must Be (female||male||any)'),
+
+    body('address').isObject().withMessage('Unit Address should be object '),
+    body('address.city')
+        .isAlpha()
+        .withMessage('Unit City should be characters'),
+
+    body('address.streetName')
+        .optional()
+        .isAlphanumeric()
+        .withMessage('Unit Street '),
+
+    body('address.buildingNumber')
+        .optional()
+        .isNumeric()
+        .withMessage('Unit Building Number should be number'),
+
+    body('unitInfo').isObject().withMessage('unitInfo should be object '),
+    body('unitInfo.description')
+        .isAlpha('en-US', { ignore: 's-.,;?' })
+        .withMessage('unitInfo.description should be characters'),
+
+    body('unitInfo.rooms')
+        .isNumeric()
+        .withMessage('unitInfo.rooms should be number'),
+
+    body('unitInfo.bathrooms')
+        .isNumeric()
+        .withMessage('unitInfo.bathrooms should be number'),
+    body('unitInfo.floor')
+        .isNumeric()
+        .withMessage('unitInfo.floor should be number'),
+
+    body('dailyPrice')
+        .isNumeric()
+        .withMessage('Unit Daily Price Must Be Number'),
+    body('isAvailable')
+        .isBoolean()
+        .withMessage(
+            'Unit Availability Must Be Added as true(available), false(unavailable)'
+        ),
+    body('isPetsAllowed')
+        .isBoolean()
+        .withMessage('Pets Allowed or Not-allowed Must Be Added ')
+        .optional(),
+    body('cover')
+        .optional()
+        .isURL()
+        .withMessage('Unit Cover must be image url.'),
+]
+
+const updateUnitValidations = [
+    body('estateType')
+        .isIn(['studio', 'shared-room', 'single-room', 'apartment'])
+        .withMessage(
+            'EstateType Must Be (Studio||Shared-Room||Single-Room||Apartment)'
+        )
+        .optional(),
+
+    body('address')
+        .isObject()
+        .withMessage('Unit Address should be object ')
+        .optional(),
+    body('address.city')
+        .isAlpha()
+        .withMessage('Unit City should be characters')
+        .optional(),
+
+    body('address.streetName').optional().isAlpha().withMessage('Unit Street '),
+
+    body('address.buildingNumber')
+        .optional()
+        .isNumeric()
+        .withMessage('Unit Building Number should be number'),
+
+    body('dailyPrice')
+        .isNumeric()
+        .withMessage('Unit Daily Price Must Be Number')
+        .optional(),
+    body('isAvailable')
+        .isBoolean()
+        .withMessage(
+            'Unit Availability Must Be Added as true(available), false(unavailable)'
+        )
+        .optional(),
+    body('isPetsAllowed')
+        .isBoolean()
+        .withMessage('Pets Allowed or Not-allowed Must Be Added ')
+        .optional(),
+]
+
+module.exports = {
+    addUnitValidations,
+    updateUnitValidations,
+}
---
+++ src/routers/validations/userValidations.js
@@ -0,0 +1,64 @@
+const { body } = require('express-validator')
+
+const userPostValidation = [
+    // body("id").isMongoId().withMessage("user id should be a number"),
+    body('fullName')
+        .isAlpha('en-US', { ignore: ' ' })
+        .withMessage('user name should be characters'),
+    body('age').isNumeric({ min: 18 }).withMessage('userAge is not valid'),
+    body('gender')
+        .isIn(['male', 'female'])
+        .withMessage('user gender is not valid'),
+    body('address').isObject().withMessage('userAddress is not valid'),
+    body('email').isEmail().withMessage('userEmail is not valid'),
+    body('phone')
+        .isAlphanumeric()
+        .withMessage('userPhone should be characters'),
+    body('password')
+        .isString({ min: 8 })
+        .isLength({ min: 8 })
+        .withMessage('password cannot be less than 8 characters'),
+    // body("national_id ").isNumeric().withMessage("user national_id  is not valid"),
+]
+
+const userUpdateValidation = [
+    // body("id").isMongoId().withMessage("user id should be a number"),
+    body('fullName')
+        .optional()
+        .isAlpha('en-US', { ignore: ' ' })
+        .withMessage('user name should be characters'),
+    body('age')
+        .isNumeric({ min: 18 })
+        .withMessage('user age is not valid')
+        .optional(),
+    body('gender')
+        .isIn(['male', 'female'])
+        .withMessage('user gender is not valid')
+        .optional(),
+    body('address')
+        .optional()
+        .isObject()
+        .withMessage('user address is not valid'),
+    body('email').isEmail().withMessage('user email is not valid').optional(),
+    body('phone')
+        .isAlphanumeric()
+        .withMessage('userPhone should be characters')
+        .optional(),
+    body('password')
+        .isString({ min: 8 })
+        .isLength({ min: 8 })
+        .withMessage('password cannot be less than 8 characters')
+        .optional(),
+    //  body("national_id ").isNumeric().withMessage("user national_id  is not valid"),
+    body('address.city')
+        .optional()
+        .isAlpha('en-US', { ignore: ' ' })
+        .withMessage('city is not valid'),
+    // body("address.street").exists({ checkFalsy: true }).withMessage("street is not valid"),
+    // body("address.building").exists({ checkFalsy: true }).withMessage("building is not valid"),
+]
+
+module.exports = {
+    userUpdateValidation,
+    userPostValidation,
+}
