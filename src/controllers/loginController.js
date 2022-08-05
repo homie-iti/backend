f99489c4a12b32f5349868edacd746de7b78a861
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-08-05 13:17:22 +0200

    fixed login issue

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -6,22 +6,38 @@ const User = require('../models/userModel')
 const appConfig = require('../config/app.config')
 const dbConfig = require('../config/database.config')
 
+// function loginError() {
+//     const error = new Error('email or password incorrect')
+//     error.status = 401
+//     return error
+// }
+
 module.exports.loginUser = (request, response, next) => {
+    console.log(request.body.email, '-', 'request.body.password')
+
     User.findOne({
         email: request.body.email,
-        password: request.body.password,
+        // password: request.body.password,
     })
-
         .then((data) => {
-            console.log(data)
+            // console.log(data)
             if (!data) {
                 const error = new Error('email or password incorrect')
                 error.status = 401
                 throw error
+                // loginError()
             } else {
                 bcrypt
                     .compare(request.body.password, data.password)
                     .then((result) => {
+                        if (!result) {
+                            const error = new Error(
+                                'email or password incorrect'
+                            )
+                            error.status = 401
+                            throw error
+                        }
+
                         const token = jwt.sign(
                             {
                                 id: data._id,
@@ -39,9 +55,11 @@ module.exports.loginUser = (request, response, next) => {
 }
 
 module.exports.loginAdmin = (request, response, next) => {
+    console.log(request.body.email, '-', 'request.body.password')
+
     Admin.findOne({
         email: request.body.email,
-        password: request.body.password,
+        // password: request.body.password,
     })
         .then((data) => {
             console.log(data)
@@ -53,6 +71,14 @@ module.exports.loginAdmin = (request, response, next) => {
                 bcrypt
                     .compare(request.body.password, data.password)
                     .then((result) => {
+                        if (!result) {
+                            const error = new Error(
+                                'email or password incorrect'
+                            )
+                            error.status = 401
+                            throw error
+                        }
+
                         const token = jwt.sign(
                             {
                                 id: data._id,
--- src/routers/loginRoute.js
+++ src/routers/loginRoute.js
@@ -3,7 +3,7 @@ const controller = require('../controllers/loginController')
 
 const route = express.Router()
 
-route.post('/login/admin', controller.loginAdmin)
 route.post('/login', controller.loginUser)
+route.post('/login/admin', controller.loginAdmin)
 
 module.exports = route
