39019df8bf0251b796ac68c93663bd4e6171a859
maryamragab <mariam.relshafei@yahoo.com>
2022-08-06 03:30:51 +0200

    using params instead body

--- src/routers/paymentRoute.js
+++ src/routers/paymentRoute.js
@@ -6,8 +6,8 @@ const validationMW = require('../middlewares/validationMW')
 const paymentController = require('../controllers/paymentController')
 
 router.post('/book-unit/:id', paymentController.bookUnit)
-router.put('/confirm-booking', paymentController.confirmBookingUnit)
+router.put('/confirm-booking/:id', paymentController.confirmBookingUnit)
 
-router.put('/cancel-booking', paymentController.cancelBookingUnit)
+router.put('/cancel-booking/:id', paymentController.cancelBookingUnit)
 
 module.exports = router
